d8106adf390edf79533e94f16557f356f714cc9e
Tyler Reitz <tdreitz@gmail.com>
2017-09-01 22:51:38 -0700

    Version 1

--- raf-basic-loop.js
+++ raf-basic-loop.js
@@ -1,31 +1,50 @@
 const getProgress = ({elapsed, total}) => Math.min(elapsed / total, 1)
 const easeOut = progress => Math.pow(--progress, 5) + 1
+const getX = () => Number(circle.getAttribute("cx"))
 
-const element = document.querySelector('span')
-const finalPosition = 1180
+const [gaussian, circle] = document.querySelectorAll('feGaussianBlur, circle')
+const startX = getX()
+const deviation = 25
 
-const time = {
-  start: performance.now(),
-  total: 2000
+const blur = start => {
+  const time = {
+    start,
+    total: 800
+  }
+
+  const tick = now => {
+    time.elapsed = now - time.start
+    const progress = deviation - deviation * getProgress(time)
+    gaussian.setAttribute('stdDeviation', `${progress}, 0`)
+    if (progress) requestAnimationFrame(tick)
+  }
+
+  requestAnimationFrame(tick)
 }
 
-const tick = now => {
-  time.elapsed = now - time.start
-  const progress = getProgress(time)
-  const easing = easeOut(progress)
-  const position = easing * finalPosition
-  
-  element.style.transform = `translate(${position}px)`
-  
-  console.table({
-    state: {
-      time: time.elapsed,
-      progress: progress,
-      position: position
-    }
-  })
-  
-  if (time.elapsed < time.total) requestAnimationFrame(tick)
+const move = () => {
+  const distance = window.innerWidth - 2 * startX
+  const backwards = getX() > startX
+
+  const time = {
+    start: performance.now(),
+    total: 1500
+  }
+
+  const tick = now => {
+    time.elapsed = now - time.start
+    
+    const progress = getProgress(time)
+    const easing = easeOut(progress) * distance
+    const delta = backwards ? distance - easing : easing
+    const cx = startX + delta
+
+    circle.setAttribute('cx', cx)
+    progress > 1 ? requestAnimationFrame(tick) : setTimeout(move, 800)
+  }
+
+  blur(time.start)
+  requestAnimationFrame(tick)
 }
 
-requestAnimationFrame(tick)
+move()
